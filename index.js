7ed423e520fec56cd64f42f0502a972dda1a09b4
Shiwei Zhang <shizh@microsoft.com>
2023-05-11 19:25:00 +0800

    feat: setup notation
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

---
+++ index.js
@@ -0,0 +1,55 @@
+const os = require('os');
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+// Map arch to go releaser arch
+// Reference: https://nodejs.org/api/os.html#os_os_arch
+function mapArch(arch) {
+  const mappings = {
+    arm: 'armv7',
+    x64: 'amd64'
+  };
+  return mappings[arch] || arch;
+}
+
+// Map os to go releaser os
+// Reference: https://nodejs.org/api/os.html#os_os_platform
+function mapOS(os) {
+  const mappings = {
+    win32: 'windows'
+  };
+  return mappings[os] || os;
+}
+
+// Get the URL to download asset
+function getDownloadURL(version) {
+  const platform = os.platform();
+  const filename = `notation_${version}_${mapOS(platform)}_${mapArch(os.arch())}`;
+  const extension = platform === 'win32' ? 'zip' : 'tar.gz';
+  return `https://github.com/notaryproject/notation/releases/download/v${version}/${filename}.${extension}`;
+}
+
+// Download and install Notation CLI of the specified version
+async function setup() {
+  try {
+    // Download Notation CLI of the specified version
+    const version = core.getInput('version');
+    const donwloadURL = getDownloadURL(version)
+    const assetPath = await tc.downloadTool(donwloadURL);
+
+    // Extract the tarball/zipball onto host runner
+    const extract = donwloadURL.endsWith('.zip') ? tc.extractZip : tc.extractTar;
+    const pathToCLI = await extract(assetPath);
+
+    // Expose the tool by adding it to the PATH
+    core.addPath(pathToCLI);
+  } catch (e) {
+    core.setFailed(e);
+  }
+}
+
+module.exports = setup
+
+if (require.main === module) {
+  setup();
+}
